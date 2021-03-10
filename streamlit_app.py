e62b34f6dc7610486193b514fdbd41ad23248181
Hennie <31654880+henniedeharder@users.noreply.github.com>
2021-03-10 21:54:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,11 @@ pred = predict(model, new_mush, encoder)
 
 # get explanation
 new = encoder.transform(new_mush)
+new
 shap_df = get_explanations(new, model)
+shap_df
 interpretation = create_interpret_df(shap_df, new, new_mush)
+interpretation
 fig, ax = plot_interpretation(interpretation)
 
 # clicking the send button
